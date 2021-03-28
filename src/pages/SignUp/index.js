41714260e393f30034d94c239968a59fd7443bef
Jhon9191 <joaoantonio15161516@gmail.com>
2021-03-27 23:49:37 -0300

    Creating register page layout

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,10 +1,39 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom'
+import styles from './SignUp.module.css'
+import Logo from '../../assets/logo.png'
+
 const Signup = () => {
+
+    const [name, setName] = useState(" ")
+    const [email, setEmail] = useState(" ")
+    const [password, setPassword] = useState(" ")
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        alert("Clicou")
+    }
+
     return (
-      <div>
-        <h1>Signup</h1>
-      </div>
+        <div className={styles.container}>
+            <div className={styles.login}>
+                <div className={styles.loginArea}>
+                    <img src={Logo} alt="logo-do-sistema" />
+                </div>
+
+                    <form onSubmit={handleSubmit}>
+                        <h1>Entrar</h1>
+                        <input onChange={(e)=>setName(e.target.valuea)} type="text" placeholder="Insira seu nome" />
+                        <input onChange={(e)=>setEmail(e.target.valuea)} type="text" placeholder="Insira seu email" />
+                        <input onChange={(e)=>setPassword(e.target.valuea)} type="password" placeholder="********" />
+                        <button type="submit">Acessar</button>
+                    </form>
+                    <Link to="/">Já possui uma conta?</Link>
+            </div>
+        </div>
     );
-  }
-  
-  export default Signup;
+}
+
+export default Signup;
+
   
