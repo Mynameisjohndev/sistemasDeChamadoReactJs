87b01a7220a7e1c8d9d65e7377ccae5b3729f769
Jhon9191 <joaoantonio15161516@gmail.com>
2021-03-27 23:24:25 -0300

    Creating login page layout

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,10 +1,32 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom'
+
+import styles from './SignIn.module.css'
+import Logo from '../../assets/logo.png'
+
 const Signin = () => {
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
     return (
-      <div>
-        <h1>Signin</h1>
-      </div>
+        <div className={styles.container}>
+            <div className={styles.login}>
+                <div className={styles.loginArea}>
+                    <img src={Logo} alt="logo-do-sistema" />
+                </div>
+
+                    <form>
+                        <h1>Entrar</h1>
+                        <input type="text" placeholder="Insira seu email" />
+                        <input type="password" placeholder="********" />
+                        <button type="submit">Acessar</button>
+                    </form>
+
+                    <Link to="/register">Criar uma nova conta</Link>
+            </div>
+        </div>
     );
-  }
-  
-  export default Signin;
-  
+}
+
+export default Signin;
