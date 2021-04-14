cca14e6bd99e5f582cb54e9e9ccdc73634df7f5e
Jhon9191 <joaoantonio15161516@gmail.com>
2021-04-14 11:42:48 -0300

    Call list

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,5 +1,5 @@
 import { useState, useContext } from 'react';
-import { FiMessageSquare, FiPlus } from 'react-icons/fi'
+import { FiMessageSquare, FiPlus, FiSearch, FiEdit2 } from 'react-icons/fi'
 import { Link } from 'react-router-dom';
 
 import { UserContext } from '../../context/user'
@@ -10,37 +10,67 @@ import './dashboard.css'
 
 const Dashboard = () => {
   const { signout } = useContext(UserContext)
-  const [chamados, setChamados] = useState([]);
+  const [chamados, setChamados] = useState([1]);
   return (
     <div>
-    <Header/>
+      <Header/>
 
-    <div className="content">
-      <Title name="Atendimentos">
-        <FiMessageSquare size={25} />
-      </Title>
+      <div className="content">
+        <Title name="Atendimentos">
+          <FiMessageSquare size={25} />
+        </Title>
 
-      {chamados.length === 0 ? (
-        <div className="container dashboard">
-          <span>Nenhum chamado registrado...</span>
+        {chamados.length === 0 ? (
+          <div className="container dashboard">
+            <span>Nenhum chamado registrado...</span>
 
-          <Link to="/new" className="new">
-            <FiPlus size={25} color="#FFF" />
-            Novo chamado
-          </Link>
-        </div>
-      )  : (
-        <>
-          <Link to="/new" className="new">
-            <FiPlus size={25} color="#FFF" />
-            Novo chamado
-          </Link>
-        </>
-      )}
+            <Link to="/new" className="new">
+              <FiPlus size={25} color="#FFF" />
+              Novo chamado
+            </Link>
+          </div>
+        )  : (
+          <>
+            <Link to="/new" className="new">
+              <FiPlus size={25} color="#FFF" />
+              Novo chamado
+            </Link>
+
+            <table>
+              <thead>
+                <tr>
+                  <th scope="col">Cliente</th>
+                  <th scope="col">Assunto</th>
+                  <th scope="col">Status</th>
+                  <th scope="col">Cadastrado em</th>
+                  <th scope="col">#</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td data-label="Cliente">Sujeito</td>
+                  <td data-label="Assunto">Suporte</td>
+                  <td data-label="Status">
+                    <span className="badge" style={{backgroundColor: '#5cb85c' }}>Em aberto</span>
+                  </td>
+                  <td data-label="Cadastrado">20/06/2021</td>
+                  <td data-label="#">
+                    <button className="action" style={{backgroundColor: '#3583f6' }}>
+                      <FiSearch color="#FFF" size={17} />
+                    </button>
+                    <button className="action" style={{backgroundColor: '#F6a935' }}>
+                      <FiEdit2 color="#FFF" size={17} />
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </>
+        )}
+
+      </div>
 
     </div>
-
-  </div>
   );
 }
 
