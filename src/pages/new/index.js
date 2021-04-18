166bbdd9979b08e9926cca3ae29f8376ef5601fd
Jhon9191 <joaoantonio15161516@gmail.com>
2021-04-18 10:41:15 -0300

    Method for choosing the subject of the call

--- src/pages/new/index.js
+++ src/pages/new/index.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 
 import Header from '../../components/Header';
 import Title from '../../components/Title';
@@ -5,81 +6,87 @@ import Title from '../../components/Title';
 import './new.css';
 import { FiPlusCircle } from 'react-icons/fi'
 
-const New = () =>{
+const New = () => {
+
+    const [assunto, setAssunto] = useState("Suporte");
 
 
-  function handleRegister(e){
-    e.preventDefault();
+    function handleRegister(e) {
+        e.preventDefault();
+        alert('TESTE')
+    }
 
-    alert('TESTE')
-  }
+    const handleOption = (e) => {
+        setAssunto(e.target.value);
+        console.log(e.target.value);
+    }
 
-  return(
-    <div>
-      <Header/>
+    return (
+        <div>
+            <Header />
 
-      <div className="content">
-        <Title name="Novo chamado">
-          <FiPlusCircle size={25} />
-        </Title>
+            <div className="content">
+                <Title name="Novo chamado">
+                    <FiPlusCircle size={25} />
+                </Title>
 
-        <div className="container">
+                <div className="container">
 
-          <form className="form-profile"  onSubmit={handleRegister} >
-            
-            <label>Cliente</label>
-            <select>
-              <option key={1} value={1}>
-               Jooj
+                    <form className="form-profile" onSubmit={handleRegister} >
+
+                        <label>Cliente</label>
+                        <select>
+                            <option key={1} value={1}>
+                                Jooj
               </option>
-            </select>
+                        </select>
 
-            <label>Assunto</label>
-            <select>
-              <option value="Suporte">Suporte</option>
-              <option value="Visita Tecnica">Visita Tecnica</option>
-              <option value="Financeiro">Financeiro</option>
-            </select>
+                        <label>Assunto</label>
+                        <select value={assunto} onChange={handleOption}>
+                            <option value="Suporte">Suporte</option>
+                            <option value="Visita Tecnica">Visita Tecnica</option>
+                            <option value="Financeiro">Financeiro</option>
+                        </select>
 
-            <label>Status</label>
-            <div className="status">
-              <input 
-              type="radio"
-              name="radio"
-              value="Aberto"
-              />
-              <span>Em Aberto</span>
+                        <label>Status</label>
+                        <div className="status">
+                            <input
+                                type="radio"
+                                name="radio"
+                                value="Aberto"
+                            />
+                            <span>Em Aberto</span>
 
-              <input 
-              type="radio"
-              name="radio"
-              value="Progresso"
-              />
-              <span>Progresso</span>
+                            <input
+                                type="radio"
+                                name="radio"
+                                value="Progresso"
+                            />
+                            <span>Progresso</span>
 
-              <input 
-              type="radio"
-              name="radio"
-              value="Atendido"
-              />
-              <span>Atendido</span>
+                            <input
+                                type="radio"
+                                name="radio"
+                                value="Atendido"
+                            />
+                            <span>Atendido</span>
+                        </div>
+
+                        <label>Complemento</label>
+                        <textarea
+                            type="text"
+                            placeholder="Descreva seu problema (opcional)."
+                        />
+
+                        <button type="submit">Registrar</button>
+
+                    </form>
+
+                </div>
+
             </div>
-
-            <label>Complemento</label>
-            <textarea
-              type="text"
-              placeholder="Descreva seu problema (opcional)."
-            />
-            
-            <button type="submit">Registrar</button>
-
-          </form>
-
         </div>
-
-      </div>
-    </div>
-  )
+    )
 }
 
 export default New;
